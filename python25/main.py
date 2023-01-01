43754a7909cbc452c77f54e8ae90f5f59a5507c5
magdark <98254681+MagaDark@users.noreply.github.com>
2023-01-01 21:47:47 +0300

    Update main.py

--- python25/main.py
+++ python25/main.py
@@ -1,16 +1,4 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+25.5. Практикум
+Задание 25.5.1.
+В написанном тесте (проверка карточек питомцев) добавьте неявные ожидания всех элементов (фото, имя питомца, его возраст).
+В написанном тесте (проверка таблицы питомцев) добавьте явные ожидания элементов страницы.
